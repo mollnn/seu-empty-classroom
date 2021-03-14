7fcf575e775790a0ab7969b37eab6a60eb8f5e8f
mollnn <1084918073@qq.com>
2021-03-14 16:19:21 +0800

    feat: 使用情况查询

--- main.cpp
+++ main.cpp
@@ -1,8 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const string pattern_begin = "YPSJDD\": \"";
-const string pattern_end = "\",";
+const string record_pattern_begin = "YPSJDD\": \"";
+const string record_pattern_end = "\",";
+
+const string daynames[] = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"};
+
+vector<string> SplitString(const string &str, char delim)
+{
+    int cur = 0;
+    vector<string> ans;
+    while (cur < str.size())
+    {
+        int pos = str.find(delim, cur);
+        if (pos < 0 || pos >= str.size())
+        {
+            string tmp = str.substr(cur);
+            ans.push_back(tmp);
+            break;
+        }
+        string tmp = str.substr(cur, pos - cur);
+        ans.push_back(tmp);
+        cur = pos + 1;
+    }
+    return ans;
+}
+
+string FindBetween(const string &str, const string &pattern_begin, const string &pattern_end)
+{
+    int pos_begin = 0;
+    if (pattern_begin.size())
+        pos_begin = str.find(pattern_begin, 0);
+    if (pos_begin < 0 || pos_begin >= str.size())
+        return "";
+    pos_begin += pattern_begin.length();
+    int pos_end = str.size();
+    if (pattern_end.size())
+        pos_end = str.find(pattern_end, pos_begin);
+    if (pos_begin >= pos_end)
+        return "";
+    string ans = str.substr(pos_begin, pos_end - pos_begin);
+    return ans;
+}
+
+struct Record
+{
+    string week;
+    string day;
+    int sec_min;
+    int sec_max;
+    string name;
+
+    Record() {}
+    Record(const string &str)
+    {
+        auto vec = SplitString(str, ' ');
+        if (vec.size() >= 4)
+        {
+            const string &str_week = vec[0];
+            const string &str_day = vec[1];
+            const string &str_sec = vec[2];
+            name = vec[3];
+            week = str_week;
+            day = str_day;
+            if (str_sec.find('-') == str_sec.npos)
+            {
+                string tmp = FindBetween(str_sec, "", "节");
+                sec_min = sec_max = atoi(tmp.c_str());
+            }
+            else
+            {
+                string tmp1 = FindBetween(str_sec, "", "-");
+                string tmp2 = FindBetween(str_sec, "-", "节");
+                sec_min = atoi(tmp1.c_str());
+                sec_max = atoi(tmp2.c_str());
+            }
+        }
+    }
+
+    bool Compare(int q_day, int q_sec)
+    {
+        if (daynames[q_day] == day && sec_min <= q_sec && q_sec <= sec_max)
+            return true;
+        return false;
+    }
+
+    bool operator<(const Record &rhs) const
+    {
+        return name < rhs.name;
+    }
+
+    friend ostream &operator<<(ostream &os, const Record &rec)
+    {
+        os << rec.week << " " << rec.day << " " << rec.sec_min << "-" << rec.sec_max << " " << rec.name;
+        return os;
+    }
+};
 
 signed main()
 {
@@ -12,24 +105,67 @@ signed main()
     buffer << t.rdbuf();
     std::string str(buffer.str());
 
-    vector<string> items;
+    map<string, string> mp_cls;
+
+    vector<Record> records;
     int cur = 0;
     while (cur < str.size())
     {
-        int pos_begin = str.find(pattern_begin, cur);
+        int pos_begin = str.find(record_pattern_begin, cur);
         if (pos_begin < 0 || pos_begin >= str.size())
             break;
-        pos_begin += pattern_begin.length();
-        int pos_end = str.find(pattern_end, pos_begin);
+        pos_begin += record_pattern_begin.length();
+        int pos_end = str.find(record_pattern_end, pos_begin);
         string ans = str.substr(pos_begin, pos_end - pos_begin);
-        items.push_back(ans);
-        cout << ans << endl;
         cur = pos_end;
+        auto vec = SplitString(ans, ',');
+        for (auto i : vec)
+        {
+            Record tmp(i);
+            records.push_back(tmp);
+            mp_cls[tmp.name] = "";
+        }
     }
-    cout << items.size() << endl;
-    for (int i = 0; i < items.size(); i++)
+
+    // Answer the queries
+
+    while (true)
     {
-        cout << items[i] << endl;
+        cout << "Enter two numbers: day and section (day -1 to exit)" << endl;
+        int day, sec;
+        cin >> day;
+        if (day == -1)
+            return 0;
+        cin >> sec;
+        cout << "--- Busy classroom list ---" << endl;
+        vector<Record> ans;
+        for (auto record : records)
+        {
+            if (record.Compare(day, sec))
+            {
+                ans.push_back(record);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        map<string, string> result = mp_cls;
+        for (auto record : ans)
+        {
+            result[record.name] += record.week + ";";
+        }
+        for (auto tmp : result)
+        {
+            auto x = tmp.first;
+            auto y = tmp.second;
+            set<string> z;
+            auto tmpvec = SplitString(y, ';');
+            for (auto i : tmpvec)
+                z.insert(i);
+            cout << x << "\t\t";
+            for (auto i : z)
+                cout << i << " ";
+            cout << endl;
+        }
     }
+
     return 0;
 }
